68b5a4fbb8a75a95711f9b4186f4acc7c04a8328
Replit user <>
2023-03-23 01:56:42 +0000

    new try

--- list.c
+++ list.c
@@ -30,7 +30,7 @@ Node * createNode(void * data) {
 
 List * createList() {
 
-  List* v = (List*) malloc(sizeof(List))
+  List* v = (List*) malloc(sizeof(List));
   if(v == NULL){
     EXIT_FAILURE;
   } 
